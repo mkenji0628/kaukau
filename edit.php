d8c75ea10e403622a77b1e5f0c094d21250d26dc
mkenji0628 <minamotojonisann@yahoo.co.jp>
2022-03-11 21:31:55 +0900

    編集画面完成

--- edit.php
+++ edit.php
@@ -6,22 +6,17 @@
 
 <body>
     <div class="wrapper">
-        <h2>商品名変更</h2>
+        <h2>商品情報変更</h2>
         <form action="" method="post">
-            
-            <input type="text" name="title">
+
+            <input type="text" name="title" placeholder="商品名を入力してください">
+            <input type="text" name="title" placeholder="値段を入力してください">
             <input type="submit" value="商品名変更" class="btn submit-btn">
         </form>
     </div>
 
     <div class="wrapper">
-        <h2>商品価格変更</h2>
-        <form action="" method="post">
-            <input type="text" name="price">
-            <input type="submit" value="価格変更" class="btn submit-btn">
-        </form>
-
-    <a href="index.php" class="btn return-btn">戻る</a>
+        <a href="index.php" class="btn return-btn">戻る</a>
     </div>
 </body>
 
