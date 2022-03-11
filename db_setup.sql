59fde2e795d642ca7a11233a58b7072d2b9bd06b
mkenji0628 <minamotojonisann@yahoo.co.jp>
2022-03-11 13:11:44 +0900

    一括で代入できる

--- db_setup.sql
+++ db_setup.sql
@@ -7,31 +7,6 @@ GRANT ALL ON kaukau_db.* TO test_user;
 
 
 
-
-CREATE TABLE IF NOT EXISTS tasks (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-	price int not null,
-    status VARCHAR(10) NOT NULL DEFAULT 'notyet',
-    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
-
-
-
-
-
-
-CREATE DATABASE IF NOT EXISTS kaukau_db;
-
-CREATE USER IF NOT EXISTS test_user
-    IDENTIFIED BY '1234';
-
-GRANT ALL ON kaukau_db.* TO test_user;
-
-
-
 CREATE TABLE IF NOT EXISTS product (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
@@ -48,14 +23,14 @@ CREATE TABLE IF NOT EXISTS product (
 
 
 
-insert into product values(null, '松の実', 700, aaa);
-insert into product values(null, 'くるみ', 270);
-insert into product values(null, 'ひまわりの種', 210);
-insert into product values(null, 'アーモンド', 220);
-insert into product values(null, 'カシューナッツ', 250);
-insert into product values(null, 'ジャイアントコーン', 180);
-insert into product values(null, 'ピスタチオ', 310);
-insert into product values(null, 'マカダミアナッツ', 600);
-insert into product values(null, 'かぼちゃの種', 180);
-insert into product values(null, 'ピーナッツ', 150);
-insert into product values(null, 'クコの実', 400);
+insert into product values(null, '松の実', 700, 'notyet');
+insert into product values(null, 'くるみ', 270, 'notyet');
+insert into product values(null, 'ひまわりの種', 210, 'notyet');
+insert into product values(null, 'アーモンド', 220, 'notyet');
+insert into product values(null, 'カシューナッツ', 250, 'notyet');
+insert into product values(null, 'ジャイアントコーン', 180), 'notyet';
+insert into product values(null, 'ピスタチオ', 310, 'notyet');
+insert into product values(null, 'マカダミアナッツ', 600, 'notyet');
+insert into product values(null, 'かぼちゃの種', 180, 'notyet');
+insert into product values(null, 'ピーナッツ', 150, 'notyet');
+insert into product values(null, 'aaa', 700, 'notyet');
