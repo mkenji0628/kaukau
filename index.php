60c5be59d95412a7e8e7456b5e75a7f17ade44ee
mkenji0628 <minamotojonisann@yahoo.co.jp>
2022-03-12 11:51:27 +0900

    リボンいじる前

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ $done_tasks = find_task_by_status(TASK_STATUS_DONE);
         </div>
         <hr>
         <div class="done-task">
-            <h2>売り切れた商品</h2>
+            <h2 class="ribbon1">売り切れた商品</h2>
             <ul>
                 <?php foreach ($done_tasks as $task) : ?>
                     <li>
